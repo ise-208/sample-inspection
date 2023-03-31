ca5b2e648b3e49c8efb9fa2a07d543a7a58dd9eb
ise-208 <seed.0ri00@gmail.com>
2023-03-31 23:30:01 +0900

    詳細画面

---
+++ lib/presentation/detail.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Detail extends StatelessWidget {
+  Function closeAction;
+
+  Detail(this.closeAction);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(0, 0, 0, 0.5),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+
+          child: Container(
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+                boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 5)]),
+            child: ,// todo ,
+          ),
+        ),
+      ),
+    );
+  }
+}
