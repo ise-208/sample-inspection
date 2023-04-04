96e6c4d6ccfc0719bada6ca16611e88f1fb60668
ise-208 <seed.0ri00@gmail.com>
2023-04-04 23:48:57 +0900

    safeareaとcolumnの実装

--- lib/presentation/sample_page2.dart
+++ lib/presentation/sample_page2.dart
@@ -3,13 +3,34 @@ import 'package:flutter/material.dart';
 class SamplePage2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: AppBar(title: Text('Sample2')),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.blue,
+              child: Text("1"),
+            ),
+            Container(
+              height: 50,
+              width: 50,
+              color: Colors.blue,
+              child: Text("2"),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print("object"),
         child: Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-    );
+    ));
   }
 }
