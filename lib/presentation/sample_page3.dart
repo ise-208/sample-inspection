5328a0f4c0f2f7eecbe9b86313f7aa67d3a92e21
ise-208 <seed.0ri00@gmail.com>
2023-04-09 05:18:19 +0900

    textの変更

--- lib/presentation/sample_page3.dart
+++ lib/presentation/sample_page3.dart
@@ -5,8 +5,50 @@ class SamplePage3 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('Sample3')),
-        body: const Center(
-          child: Text("Hello"),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                child: Text("Hello Sample3"),
+              ),
+              Container(
+                width: double.infinity,
+                child: const Text(
+                  "Hello Center",
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                child: const Text(
+                  "Hello Right",
+                  textAlign: TextAlign.right,
+                  style: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              DefaultTextStyle(
+                style: _commonTextStyle,
+                child: Container(
+                  child: Column(
+                    children: const [
+                      Text("1"),
+                      Text("2"),
+                      Text("3"),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ));
   }
+
+  static const _commonTextStyle = TextStyle(
+    color: Colors.green,
+    fontFamily: 'Roboto',
+    fontWeight: FontWeight.w900,
+  );
 }
