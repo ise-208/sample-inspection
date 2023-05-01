68de68640b4df11c69b6748cb1271bab92b3bab0
ise-208 <seed.0ri00@gmail.com>
2023-05-01 22:23:56 +0900

    slideの実装

--- lib/presentation/sample|_page6.dart
+++ lib/presentation/sample|_page6.dart
@@ -9,6 +9,22 @@ class _SamplePage6 extends State {
   bool bar = false;
   bool foo = false;
 
+  double _value = 0.0;
+  double _startValue = 0.0;
+  double _endValue = 0.0;
+
+  void _changeSlide(double e) => setState(() {
+        _value = e;
+      });
+
+  void _startSlide(double e) => setState(() {
+        _startValue = e;
+      });
+
+  void _endSlide(double e) => setState(() {
+        _endValue = e;
+      });
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,6 +69,25 @@ class _SamplePage6 extends State {
                 height: 50,
                 child: Text("ExpansionTile"),
               )
+            ],
+          ),
+          ExpansionTile(
+            title: const Text("説明"),
+            children: <Widget>[
+              Container(
+                  height: 50,
+                  child: Slider(
+                    min: 0,
+                    max: 10,
+                    activeColor: Colors.green,
+                    inactiveColor: Colors.red,
+                    divisions: 10,
+                    label: '${_value}',
+                    value: _value,
+                    onChanged: _changeSlide,
+                    onChangeStart: _startSlide,
+                    onChangeEnd: _endSlide,
+                  ))
             ],
           ),
         ],
