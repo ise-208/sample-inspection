7f55cc01bb44f532d2639f4aa95614f6187499c2
ise-208 <seed.0ri00@gmail.com>
2023-05-11 23:42:34 +0900

    GridViewの実装

--- lib/presentation/sample_page1.dart
+++ lib/presentation/sample_page1.dart
@@ -5,6 +5,27 @@ class SamplePage1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Page1")),
+      body: GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 4,
+        mainAxisSpacing: 5,
+        shrinkWrap: true,
+        children: List.generate(
+            20,
+            (index) => Container(
+                  padding: EdgeInsets.all(8),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(color: Colors.green),
+                  child: GridTile(
+                    child: Icon(Icons.add),
+                    footer: Center(
+                      child: Text(
+                        "Message $index",
+                      ),
+                    ),
+                  ),
+                )),
+      ),
     );
   }
 }
