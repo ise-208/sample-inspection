82d482f0f907215ff61b108b861bdae0ead0eaad
ise-208 <seed.0ri00@gmail.com>
2023-05-18 01:07:37 +0900

    AppsDrainage

--- lib/presentation/sample_page8.dart
+++ lib/presentation/sample_page8.dart
@@ -27,6 +27,7 @@ class _SamplePage8 extends State {
           children: [
             OptimizerButton(),
             BatteryLevelIndicatorPainter(),
+            AppsDrainage(),
           ],
         ),
       ),
@@ -140,6 +141,94 @@ class BatteryLevelIndicatorPainter extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class _HorizontalBorder extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 1,
+      margin: EdgeInsets.symmetric(horizontal: 16),
+      color: Colors.grey[200],
+    );
+  }
+}
+
+class _AppColumn extends StatelessWidget {
+  final String name;
+  final Icon icon;
+  final String percentage;
+
+  const _AppColumn({
+    Key? key,
+    required this.name,
+    required this.icon,
+    required this.percentage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: icon,
+      title: Text(
+        name,
+        style: TextStyle(color: kColorText),
+      ),
+      trailing: Text(
+        percentage,
+        style: TextStyle(color: kColorText),
+      ),
+    );
+  }
+}
+
+class AppsDrainage extends StatelessWidget {
+  const AppsDrainage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          leading: ClipOval(
+            child: Container(
+              color: kColorPurple,
+              child: Icon(Icons.apps, color: Colors.white),
+            ),
+          ),
+          title: Text(
+            "Drainage",
+            style: TextStyle(color: kColorTitle),
+          ),
+          subtitle: Text(
+            "app",
+            style: TextStyle(color: kColorText),
+          ),
+        ),
+        Material(
+          color: Colors.white,
+          elevation: kElevation,
+          borderRadius: BorderRadius.circular(16),
+          child: Column(
+            children: [
+              const _AppColumn(
+                name: "Aapp",
+                icon: Icon(Icons.sms),
+                percentage: "75%",
+              ),
+              _HorizontalBorder(),
+              const _AppColumn(
+                name: "Bapp",
+                icon: Icon(Icons.live_tv, color: Colors.red),
+                percentage: "75%",
+              ),
+              _HorizontalBorder(),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
